96d0348cb753401d8d080a9d28ef2921d7850df9
MasRama <ryushineo@gmail.com>
2023-11-06 18:26:31 +0700

    initial commit

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rama - Laravel</title>
+</head>
+<body>
+    <h1>Hello World !</h1>
+    <h3>Fatqan Ramadhiansyah</h3>
+    <h3>ryushineo@gmail.com</h3>
+    <h3>SIB PT Arkatama Multisolusindo</h3>
+    <p>Belajar Laravel</p>
+</body>
+</html>
